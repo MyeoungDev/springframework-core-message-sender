fc29a98ba7fe7596d25dba570337ebe9c6256a60
MyeoungDev <73057935+MyeoungDev@users.noreply.github.com>
2022-10-12 13:22:37 +0900

    custom Annotation practice

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSenderService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSenderService.java
@@ -1,24 +1,25 @@
 package com.nhnacademy.edu.springframework.messagesender.service;
 
 import com.nhnacademy.edu.springframework.messagesender.User;
+import com.nhnacademy.edu.springframework.messagesender.annotation.SMS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 public class MessageSenderService {
 
-    private MessageSender messageSender;
+    private final MessageSender messageSender;
+    private final String from;
 
-//    public MessageSenderService(MessageSender messageSender) {
-//        this.messageSender = messageSender;
-//    }
-
-    public MessageSenderService() {
-
-    }
-
-    public void setEmailMessageSender(MessageSender messageSender) {
+//    @SMS
+    public MessageSenderService(@SMS("smsMessageSender") MessageSender messageSender,
+                                @Value("${from}") String from) {
         this.messageSender = messageSender;
+        this.from = from;
     }
-
     public void send() {
+        System.out.println("from : " + from);
         messageSender.sendMessage(new User("test@test", "010"), "message");
     }
 }
